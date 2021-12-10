34a6425fcb1e12ff2f627c760796a0aed0503212
ManuelaDeLaHoz <91165865+ManuelaDeLaHoz@users.noreply.github.com>
2021-12-10 10:52:42 -0500

    Delete Proyecto directory

--- Proyecto/Documentacion/html/search/all_1.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['db_5flocal_1',['DB_Local',['../class_d_b___local.html',1,'']]]
-];
--- Proyecto/Documentacion/html/search/classes_0.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['db_5flocal_3',['DB_Local',['../class_d_b___local.html',1,'']]]
-];
